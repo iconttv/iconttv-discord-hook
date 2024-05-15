cdc0ba8822bd7ce4658711e10bf7583463f97e05
k123s456h <k123s456h@gmail.com>
2024-05-15 18:39:40 +0900

    add ts param to icon url

--- src/utils/iconttv.ts
+++ src/utils/iconttv.ts
@@ -1,14 +1,22 @@
 export function getIconttvUrl(urlpath: string): string {
-  const ICONTTV_IMAGE_PREFIX = "https://api.probius.dev/twitch-icons/cdn";
+  const ICONTTV_IMAGE_PREFIX = 'https://api.probius.dev/twitch-icons/cdn';
 
-  if (urlpath.startsWith("http://") || urlpath.startsWith("https://")) {
-    return urlpath;
-  }
-  if (urlpath.startsWith("./")) {
-    return `${ICONTTV_IMAGE_PREFIX}/${urlpath.slice(2)}`;
-  }
-  if (urlpath.startsWith("/")) {
-    return `${ICONTTV_IMAGE_PREFIX}/${urlpath.slice(1)}`;
-  }
-  return `${ICONTTV_IMAGE_PREFIX}/${urlpath}`;
+  const urlString = (() => {
+    if (urlpath.startsWith('http://') || urlpath.startsWith('https://')) {
+      return urlpath;
+    }
+    if (urlpath.startsWith('./')) {
+      return `${ICONTTV_IMAGE_PREFIX}/${urlpath.slice(2)}`;
+    }
+    if (urlpath.startsWith('/')) {
+      return `${ICONTTV_IMAGE_PREFIX}/${urlpath.slice(1)}`;
+    }
+    return `${ICONTTV_IMAGE_PREFIX}/${urlpath}`;
+  })();
+
+  const url = new URL(urlString);
+
+  url.searchParams.set('ts', `${Date.now()}`);
+
+  return url.toString();
 }
