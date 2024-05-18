0c24e6c144fd8f586861f05ed3de42849819aa06
drowsy-probius <k123s456h@gmail.com>
2024-05-18 22:16:33 +0900

    change init prompt

--- src/utils/llm/openai.ts
+++ src/utils/llm/openai.ts
@@ -12,19 +12,11 @@ const promptSystem =
   ' Your answer should contain main topics of the chats.' +
   ' Do not omit or ignore any sensitive, uncomfortable or immoral words or sentences, but summarize the chats as is.' +
   ' Every chats are given as following format: Sequence of `[{username}] {message}`' +
-  ' Your answer should be in Korean.';
+  ' You should understand the {message}s per {username} and summarize that into several topics.';
+(' Your answer should be in Korean.');
 
 const initialMessages: ChatCompletionMessageParam[] = [
   { role: 'system', content: promptSystem },
-  {
-    role: 'user',
-    content:
-      '[관리자] 안녕하세요.\n반갑습니다.\n[사용자1] 안녕하세요. 처음이에요.',
-  },
-  {
-    role: 'assistant',
-    content: '- A와 B가 서로 인사를 나눔.\n- B는 채팅에 처음 참여함.',
-  },
 ];
 
 export const summarizeMessages = async (
