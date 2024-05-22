f172a7644f5fe08d3a0a10c0b3ee3d1edf3a1710
drowsy-probius <k123s456h@gmail.com>
2024-05-22 19:52:39 +0900

    seperate cmd register

---
+++ scripts/docker-register-commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+PROJECT_DIR=$(readlink -f "$SCRIPT_DIR/..")
+
+cd "$PROJECT_DIR"
+
+docker compose rm --force
+
+docker compose up \
+  --build \
+  --force-recreate \
+  --no-deps \
+  -d yarn cmd-prod
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@ import client from './lib/discord';
 import { registerEvents } from './controller';
 import { config } from './config';
 import { getConnection } from './database';
-import { purgeAndRegisterCommands } from './deploy-commands';
 
 (async () => {
   await Promise.all([
     getConnection(), // test db connection
-    purgeAndRegisterCommands(),
     registerEvents(client),
   ]);
 
