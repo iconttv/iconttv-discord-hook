60bcf7294c71e14af221b0f00fdce6c33b6f4612
drowsy-probius <k123s456h@gmail.com>
2024-08-18 13:28:13 +0900

    add memory used log

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-import v8 from 'v8';
 import client from './lib/discord.js';
 import { registerEvents } from './controller/index.js';
 import { config } from './config.js';
 import { getConnection } from './database/index.js';
 import logger from './lib/logger.js';
 
-(async () => {
-  logger.info(
-    `Max Old Space Size: ${
-      v8.getHeapStatistics().heap_size_limit / 1024 / 1024
-    } MB`
-  );
+const logMemoryUsage = () => {
+  const mu = process.memoryUsage();
+  // # bytes / KB / MB / GB
+  const gbTotal = mu.heapTotal / 1024 / 1024 / 1024;
+  const gbNow = mu.heapUsed / 1024 / 1024 / 1024;
+  const gbRounded = Math.round(gbNow * 100) / 100;
+  logger.debug(`Heap allocated ${gbRounded} / ${gbTotal} GB`);
+};
 
+(async () => {
   await Promise.all([
     getConnection(), // test db connection
     registerEvents(client),
   ]);
 
   client.login(config.DISCORD_BOT_TOKEN);
+
+  setInterval(logMemoryUsage, 30 * 1000);
 })();
