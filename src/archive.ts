2ffeaa4bc327c19ae81ee9e1c2f4cf87a6c21ec1
drowsy-probius <k123s456h@gmail.com>
2025-11-18 01:29:47 +0900

    add kafka producer disconnect when process exit

--- src/archive.ts
+++ src/archive.ts
@@ -2,6 +2,7 @@ import client from './lib/discord';
 import { registerEventsArchive } from './controller/archive/index';
 import { config } from './config';
 import { createMongooseConnection } from './database/index';
+import { getKafkaProducer } from './service/kafkaService';
 
 (async () => {
   await Promise.all([
@@ -10,4 +11,20 @@ import { createMongooseConnection } from './database/index';
   ]);
 
   client.login(config.DISCORD_BOT_TOKEN);
+
+  [
+    `exit`,
+    `SIGINT`,
+    `SIGUSR1`,
+    `SIGUSR2`,
+    `uncaughtException`,
+    `SIGTERM`,
+  ].forEach(eventType => {
+    process.on(eventType, async () => {
+      const kafkaProducer = await getKafkaProducer();
+      if (kafkaProducer) {
+        await kafkaProducer.disconnect();
+      }
+    });
+  });
 })();
